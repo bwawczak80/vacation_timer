0084197c04f11409fb41216bb8f43d3b8467ff76
bwawczak80 <35830914+bwawczak80@users.noreply.github.com>
2019-10-02 23:56:36 -0500

    Add files via upload
    fixed errors

--- led_on_off_1.py
+++ led_on_off_1.py
@@ -1,35 +1,38 @@
-#1 /usr/bin/python
 import RPi.GPIO as GPIO
+import time
 import sys
 
-
 ledPins = [17,18,27,22,23,24,25,2,3,8]
+pin_string = sys.argv[1]
+pin_int = int(pin_string)
+arg_index = ledPins.index(pin_int)
+pin = ledPins[arg_index]
 
-pin = sys.argv[2]
-int_pin = int(pin)
-pin_num = ledPins[int_pin]        
-
-if (sys.argv[1] == 'on'):
+if (sys.argv[2] == 'on'):
     switch = GPIO.LOW
 else:
     switch = GPIO.HIGH
-    
+
+
 def setup():
+    print("Program is starting......")
     GPIO.setmode(GPIO.BCM)
     for pin in ledPins:
         GPIO.setup(pin, GPIO.OUT)
         GPIO.output(pin, GPIO.HIGH)
+        
         
 def destroy():
     for pin in ledPins:
         GPIO.output(pin, GPIO.HIGH)
     GPIO.cleanup()
     
+    
 def loop():
     while True:
-        
-        GPIO.output(pin_num, switch)
+        GPIO.output(pin, switch)
 
+        
 if __name__ == '__main__':
     setup()
     try:
